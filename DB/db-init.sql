43e07ac8335e9ce6bc3e12babb00a4ff1f87b328
Piotr <p.zimirski@gmail.com>
2023-05-01 13:20:14 +0200

    Configure postgres dockerisation

--- DB/db-init.sql
+++ DB/db-init.sql
@@ -1,17 +1,3 @@
-
--- Drop if the same DB exists
-DROP DATABASE ioproject;
-
--- Create DB
-CREATE DATABASE ioproject WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE_PROVIDER = libc LOCALE = 'en_US.utf8';
-
--- Connect to DB
-\connect ioproject;
-
--- Crate the admin user with full permissions
-CREATE ROLE admin_user WITH LOGIN PASSWORD 'admin_pass' CREATEDB CREATEROLE;
-GRANT ALL PRIVILEGES ON DATABASE ioproject TO admin_user;
-
 -- Crate the regular user with write and read permissions
 CREATE ROLE regular_user WITH LOGIN PASSWORD 'regular_pass';
 GRANT CONNECT ON DATABASE ioproject TO regular_user;
