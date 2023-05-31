426af5675fc8f13e037c127f363d8bd36c274dbc
jackpie <66798628+jackpie2@users.noreply.github.com>
2023-05-31 21:40:30 +0200

    modified catalog handling
    modified catalog handling
    
    fixed tests
    
    always pop catalog id
    
    fixed title key error
    
    added catalog tests
    
    save catalogs

--- API/api/models/catalog.py
+++ API/api/models/catalog.py
@@ -4,3 +4,10 @@ from api import db
 class CatalogModel(db.Model):
     __tablename__ = 'catalog'
     catalog_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    created_date = db.Column(db.DateTime, nullable=False)
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+        return self.catalog_id
